70db03ce43b817c7245b7c2fff9b1cba70962796
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-03-15 09:22:09 -0300

    feat: created core components

---
+++ packages/docs/vite.config.js
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+import react from '@vitejs/react'
+import { defineConfig } from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,37 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  globalCss,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
